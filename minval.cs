46f020d33355c4b8a563acf82ead6a93784e1d9e
sowmya-2004 <128731224+sowmya-2004@users.noreply.github.com>
2025-03-08 13:51:23 +0530

    Add files via upload

---
+++ minval.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_assignment
+{
+
+    class hello
+    {
+        public static void Main(string[] args)
+        {
+            string s = null;
+            try
+            {
+                Console.WriteLine(s[0]);
+            }
+            catch(Exception ex)
+            {
+                throw new NullReferenceException("null pointer");
+            }
+            finally
+            {
+                Console.WriteLine("program is completed");
+            }
+        }
+    }
+}
